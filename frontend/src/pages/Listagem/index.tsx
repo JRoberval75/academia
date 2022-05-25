788b69a6b808469f5cff88440d8044f87301be30
Roberval Silva <socram.r75@gmail.com>
2022-05-25 10:53:15 -0300

    Carde

--- frontend/src/pages/Listagem/index.tsx
+++ frontend/src/pages/Listagem/index.tsx
@@ -1,9 +1,36 @@
+import Carde from "components/Carde";
 import Paginacao from "components/Paginacao";
 
 function Listagem() {
 
     return (
-        <Paginacao />
+        <>
+            <Paginacao />
+
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <Carde />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <Carde />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <Carde />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <Carde />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <Carde />
+                    </div>
+                </div>
+            </div>
+
+
+
+
+        </>
     );
 }
 
